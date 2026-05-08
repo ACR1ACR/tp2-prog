ee2d3dfe66f178d9ba9e0c76887f918e9a42aab4
Xavier Cordero <e2541210@cmaisonneuve.qc.ca>
2026-05-08 12:55:52 -0400

    commit exo 1

--- exo1.py
+++ exo1.py
@@ -1,1 +1,13 @@
-print("Hello, World!")
+import json
+import csv
+
+with open("data.json", "r", encoding="utf-8") as f:
+    data = json.load(f)
+
+with open("resultat.csv", "w", newline="", encoding="utf-8") as f:
+    writer = csv.writer(f)
+
+    writer.writerow(["reel", "imaginaire"])
+
+    for nombre in data:
+        writer.writerow(nombre)
