dbef50bc74cc87eff36ac6286a5b7bbe9fdcb124
Armand <29armandcr@gmail.com>
2026-05-14 20:51:52 -0400

    commit

--- exo2.py
+++ exo2.py
@@ -1,1 +1,27 @@
-jyfh
+import csv
+
+def charger_pokémons_csv(fichier):
+
+    pokemons = {}
+
+    with open(fichier, "r", encoding="utf-8") as f:
+
+        lecteur_csv = csv.reader(f)
+
+        for ligne in lecteur_csv:
+
+            nom = ligne[0]
+
+            stats = []
+
+            for valeur in ligne[1:]:
+                stats.append(int(valeur))
+
+            pokemons[nom] = stats
+
+    return pokemons
+
+
+resultat = charger_pokémons_csv("pokemon.csv")
+
+print(resultat)
